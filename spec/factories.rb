48e91656bbad5a3237baddc51ab2d8c61e97f18c
jennellew <jennelle.wy.wong@gmail.com>
2012-06-25 22:25:44 -0400

    included authorization for users to edit own pages

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do 
   factory :user do
     name "Jennelle Wong"
-    email "jwong@example.com"
+    email "jw2wong@example.com"
     password "qwerty"
     password_confirmation "qwerty"
   end
